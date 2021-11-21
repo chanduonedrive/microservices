db779586acc0bf0496659922b386a90da8462843
Chandu <chanduonedrive@gmail.com>
2021-11-22 01:02:52 +0530

    initial commit

---
+++ zomato-orchestrator-saga/src/main/java/com/zomato/integration/ZomatoOrchestratorSagaApplication.java
@@ -0,0 +1,13 @@
+package com.zomato.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoOrchestratorSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoOrchestratorSagaApplication.class, args);
+	}
+
+}
---
+++ zomato-orchestrator-saga/src/test/java/com/zomato/integration/ZomatoOrchestratorSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zomato.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoOrchestratorSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
